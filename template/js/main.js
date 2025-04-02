df47bb09c9770e37c89deaddc128de7b0cd57c4f
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2025-04-03 01:16:24 +0400

    feat: add updateDynamicProperty function & dropdown menu styles

--- template/js/main.js
+++ template/js/main.js
@@ -1,29 +1,58 @@
 // burger-меню
 const mp = new MobilePanel({
-  'navbar': '.nav-bar'
+    navbar: ".nav-bar",
+});
+
+// Универсальная функция для обновления любого свойства CSS в зависимости от ширины окна с любыми единицами изменения
+function updateDynamicProperty(
+    propertyName,
+    minValue,
+    maxValue,
+    unit,
+    minWidth,
+    maxWidth
+) {
+    const windowWidth = Math.min(window.innerWidth, maxWidth);
+
+    let value =
+        minValue +
+        ((windowWidth - minWidth) * (maxValue - minValue)) /
+            (maxWidth - minWidth);
+    document.documentElement.style.setProperty(
+        `--${propertyName}`,
+        `${value}${unit}`
+    ); // unit - единица измерения
+}
+// динамическое изменение margin-right в .nav__icon--open
+window.addEventListener("load", () => {
+    updateDynamicProperty("margin-right", 6, 47, "rem", 360, 1023);
+});
+
+window.addEventListener("resize", () => {
+    updateDynamicProperty("margin-right", 6, 47, "rem", 360, 1023);
 });
 
 // Swiper слайдер
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".swiper-container", {
-      loop: true,
-      initialSlide: 1,
-      slidesPerView: 1,
-      spaceBetween: 15,
-      centeredSlides: true,
-      navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          dynamicBullets: true,
-          dynamicMainBullets: 1,
-      },
-      a11y: {
-        prevSlideMessage: 'Предыдущий слайд',
-        nextSlideMessage: 'Следующий слайд',
-      },
-  });
+    const swiper = new Swiper(".swiper-container", {
+        loop: true,
+        initialSlide: 1,
+        slidesPerView: 1,
+        spaceBetween: 15,
+        centeredSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+            dynamicMainBullets: 1,
+        },
+        a11y: {
+            prevSlideMessage: "Предыдущий слайд",
+            nextSlideMessage: "Следующий слайд",
+        },
+    });
 });
